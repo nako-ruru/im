42d08f5107882e1c888edb13aa44bdf989d95c96
nakoruru <nako_ruru@163.com>
2017-06-06 00:33:24 +0800

    java doc

--- connector/src/test/java/com/mycompany/im/connector/MessageUtils.java
+++ connector/src/test/java/com/mycompany/im/connector/MessageUtils.java
@@ -1,7 +1,10 @@
 package com.mycompany.im.connector;
 
+import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -9,10 +12,27 @@ import java.nio.charset.StandardCharsets;
  */
 public class MessageUtils {
 
+    /**
+     * 对当前{@link java.net.Socket}连接注册userId。该方法在每次连接后调用且仅调用一次
+     * @param out
+     * @param userId
+     * @throws IOException
+     *
+     * @see Socket#getInputStream()
+     * @see DataInputStream#DataInputStream(InputStream)
+     */
     public static void register(DataOutput out, String userId) throws IOException {
         writeCharSequence(out, userId);
     }
 
+    /**
+     * 普通聊天
+     * @param out
+     * @param roomId
+     * @param content
+     * @param level
+     * @throws IOException
+     */
     public static void chat(DataOutput out, String roomId, String content, int level) throws IOException {
         writeCharSequence(out, roomId);
         out.writeInt(level);
@@ -20,12 +40,27 @@ public class MessageUtils {
         writeCharSequence(out, content);
     }
 
+    /**
+     * 点赞
+     * @param out
+     * @param roomId
+     * @param level
+     * @throws IOException
+     */
     public static void support(DataOutput out, String roomId, int level) throws IOException {
         writeCharSequence(out, roomId);
         out.writeInt(level);
         out.writeInt(2);
     }
 
+    /**
+     * 发送礼物
+     * @param out
+     * @param roomId
+     * @param giftId
+     * @param level
+     * @throws IOException
+     */
     public static void sendGift(DataOutput out, String roomId, String giftId, int level) throws IOException {
         writeCharSequence(out, roomId);
         out.writeInt(level);
@@ -33,19 +68,33 @@ public class MessageUtils {
         writeCharSequence(out, giftId);
     }
 
+    /**
+     * 分享
+     * @param out
+     * @param roomId
+     * @param level
+     * @throws IOException
+     */
     public static void share(DataOutput out, String roomId, int level) throws IOException {
         writeCharSequence(out, roomId);
         out.writeInt(level);
         out.writeInt(5);
     }
 
+    /**
+     * 升级
+     * @param out
+     * @param roomId
+     * @param level
+     * @throws IOException
+     */
     public static void levelUp(DataOutput out, String roomId, int level) throws IOException {
         writeCharSequence(out, roomId);
         out.writeInt(level);
         out.writeInt(6);
     }
 
-    public static void writeCharSequence(DataOutput out, String chars) throws IOException {
+    private static void writeCharSequence(DataOutput out, String chars) throws IOException {
         byte[] bytes = chars.getBytes(StandardCharsets.UTF_8);
         out.writeInt(bytes.length);
         out.write(bytes);
